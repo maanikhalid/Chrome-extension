b127cb0c3d13810b28f732dc3221a2b5127c6827
Khalid <52281551+maanikhalid@users.noreply.github.com>
2023-03-06 17:43:15 +0000

    using .createElement
    This is the first time baby!!!
    
    .createElement is a powerful thing. Might enjoy this function more than i expect.

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-let myLeads = []
+let myLeads = ["www.google.com","www.yahoo.com", "www.msn.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ulEl")
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     console.log(myLeads)
+    
 })
 
+for (i = 0 ; i < myLeads.length ; i++){
+   
+    const myLeadsItem = document.createElement("li")
+    myLeadsItem.textContent = myLeads[i]
+    ulEl.appendChild(myLeadsItem)
+}
 
