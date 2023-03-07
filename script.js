ee3aee953c10863a40508d6bc2d9e63e59bd3b6a
Khalid <52281551+maanikhalid@users.noreply.github.com>
2023-03-07 13:47:09 +0000

    Added error messages

--- script.js
+++ script.js
@@ -3,18 +3,38 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ulEl")
 
+inputEl.placeholder = "Enter text"
+
 inputBtn.addEventListener("click", function(){
+    
+    if (inputEl.value ===""){
+        
+        inputEl.classList.add("error")
+        inputEl.placeholder = "Error: type something"
+        
+    }else{
+        
     myLeads.push(inputEl.value)
     console.log(myLeads)
-    
+    inputEl.value = ""
+    inputEl.placeholder = "Enter text"
+    inputEl.classList.remove("error")
     renderLeads()
+   }     
 })
 
+function clearInput(){
+    if (inputEl.value != ""){
+        inputEl.value = "";
+    }
+}
 
 function renderLeads () {
     let listItems = ""
     for (i = 0 ; i < myLeads.length ; i++){
-        listItems += "<li>" + myLeads[i] + "</li>"
+        // listItems += "<li><a href='http://"+myLeads[i]+"' target='_blank'>" + myLeads[i] + "</a>" + "</li>"
+        listItems += "<li><a href='http://"+myLeads[i]+"' target='_blank'>" + myLeads[i] + "</a>" + "</li>"
+        
     }
 
     ulEl.innerHTML = listItems
